1fa2b58967800c21b6141b460acf67f3cfe556e4
Danny Burt <BurtDaniel@Gmail.com>
2014-10-17 22:43:01 -0700

    created game files

--- w2d5/poker/spec/game_spec.rb
+++ w2d5/poker/spec/game_spec.rb
@@ -1,6 +1,11 @@
 require 'rspec'
+require './lib/game.rb'
 require './lib/player.rb'
+require './lib/deck.rb'
 
 describe "game" do
-  "hello!"
+  let(:game) { Game.new }
+  it "holds the deck" do
+    expect(game.deck).to eq(Deck.new)
+  end
 end
