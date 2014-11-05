ee4cd55a6516799e5cb25c769da3b616b85b0705
Daniel <BurtDaniel@Gmail.com>
2014-11-05 13:59:20 -0800

    removed rogue assessment

--- a04/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :user_id, :title, :body, presence: true
-  belongs_to :user
-  has_many :tags
-end
--- a04/config/routes.rb
+++
@@ -1,6 +0,0 @@
-PostsApp::Application.routes.draw do
-  resources :users
-  resource :session
-  resources :posts
-  resources :tags
-end
